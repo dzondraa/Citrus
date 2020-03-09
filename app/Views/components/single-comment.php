b74407b573f0d76eb04dc7cf3044fe7ac5e9d13c
dzondraa <djole.nic@gmail.com>
2020-03-09 19:32:02 +0100

    Created single-comment component

---
+++ app/Views/components/single-comment.php
@@ -0,0 +1,14 @@
+<li class="media">
+    <a class="pull-left" href="#">
+        <img class="media-object img-circle" src="https://s3.amazonaws.com/uifaces/faces/twitter/dancounsell/128.jpg" alt="profile">
+    </a>
+    <div class="media-body">
+        <div class="well well-lg">
+            <h4 class="media-heading text-uppercase reviews">Marco </h4>
+            <p class="media-comment">
+            Great snippet! Thanks for sharing.
+            </p>
+        </div>              
+    </div>
+
+</li> 
